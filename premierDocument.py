5cabdcebb72c047a864b3d260381a470b0c9ac5c
Sade El Moudden <sade.elmoudden@edu.gbsl.ch>
2026-03-01 22:58:18 +0100

    Add Firbase to Python part 1

--- premierDocument.py
+++ premierDocument.py
@@ -1,1 +1,13 @@
-print ("Hello world!")
+#import the required modules 
+import firebase_admin
+from firebase_admin import db, credentials 
+
+#authenticate to firebase
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://tm-sade-default-rtdb.europe-west1.firebasedatabase.app/"})
+
+
+
+
+
+#DON'T FORGET TO DO GIT COMMIT AT THE END (otherwise you're cooked) :)
